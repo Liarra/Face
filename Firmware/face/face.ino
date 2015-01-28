00a830784fb54431a6fafb4f4b075798289c5b03
Roy Berkeveld <roy@tnode.net>
2015-01-28 20:51:32 +0100

    Fixed DTMC implementation, added test face

--- Firmware/face/face.ino
+++ Firmware/face/face.ino
@@ -1,5 +1,7 @@
 /* Serial-controlled Face */
 
+#define debug true
+
 int led_pins[16] = {8,A0,A2,A3,A1,6,5,11,10,9,A5,A4,4,7,13,12};
 int button_pins[3] = {2,A6,A7};
 int speaker_pin = 3;
@@ -10,13 +12,13 @@ uint8_t speed = 100; // 0.1s time granularity, all time is expressed as a multip
 struct State {
     uint16_t face;
     uint8_t delay;
-    uint8_t sound;
+    uint8_t sound; // Reserved, index in sounds table
 } state;
 
 struct Transition {
     uint8_t from;
-    uint8_t p;
     uint8_t next;
+    uint8_t p;
 } transition;
 
 State states[32];
@@ -25,44 +27,41 @@ uint8_t state_count = 0;
 uint8_t transition_count = 0;
 
 // Linear feedback shift register, a 16-bit PRNG with period 2^16
-uint16_t lfsr = 0;
-void lfsr_next(void) { // Advance LFSR, gets next 16 pseudorandom bits
+uint16_t lfsr = 0xACE1;
+void lfsrNext(void) { // Advance LFSR, gets next 16 pseudorandom bits
     // taps: 16 14 13 11; characteristic polynomial: x^16 + x^14 + x^13 + x^11 + 1
     lfsr = (lfsr >> 1) ^ (-(lfsr & 1u) & 0xB400);
 }
 
 // Update face, to be called every speed ms
-void face_advance(void) {
+void faceAdvance(void) {
     static uint8_t delay;
     static uint8_t cur_state = 0;
-    
-    //~ printf("delay = %d\r", delay);
-    
+
     int i;
-    uint16_t ptotal = 0; // sum of probabilities
-    uint16_t pnow = 0;
     
     // Stay in current state for the right duration
     if (delay > 0) {
         delay--;
         return;
     }
-    printf("transition\r\n");
     
-    // Transition into next state, Markov chain logic
+    // Transition into next state, DTMC logic
+    uint16_t ptotal = 0; // sum of probabilities
     for (i=0; i<transition_count; i++) {
         if (transitions[i].from == cur_state) {
             ptotal += transitions[i].p;
         }
     }
-    lfsr_next();
-    pnow = lfsr % ptotal;
+    lfsrNext();
+    uint16_t pnow = lfsr % ptotal; // Uniform random choice
     for (i=0; i<transition_count; i++) {
         if (transitions[i].from == cur_state) {
+            //~ printf("eval %x, %d, %d, %d\r\n", lfsr, ptotal, transitions[i].p, pnow);
             if (transitions[i].p > pnow) {
+                if (debug) printf("Transition %d -(%d)-> %d\r\n", cur_state, i, transitions[i].next);
                 cur_state = transitions[i].next;
-                printf("into %d\r\n", cur_state);
-                output_face(states[cur_state].face);
+                faceOutput(states[cur_state].face);
                 delay = states[cur_state].delay;
                 break;
             }
@@ -71,11 +70,9 @@ void face_advance(void) {
     }
 }
 
-void output_face(uint16_t face) {
-    printf("setting face %d\r\n", face);
+void faceOutput(uint16_t face) {
     for (int i=0; i<16; i++) {
-        printf(" %d", i, (face>>i)&1);
-        digitalWrite(led_pins[i], (face>>i)&1);
+        digitalWrite(led_pins[i], !((face>>i)&1));
     }
 }
 
@@ -116,7 +113,7 @@ int readLine(int readch, char *buffer, int len) {
 }
 
 // Reads the line until the end or as far as it can, does not block
-inline bool readyLine(char *buffer, int len) {
+bool readyLine(char *buffer, int len) {
     char c = Serial.read();
     while (c>0) {
         if (readLine(c, buffer, len) > 0) {
@@ -134,7 +131,7 @@ inline void nextLine(char *buffer, int len) {
 
 // Direct printf to Serial.write()
 static FILE uartout = {0};
-static int uart_putchar(char c, FILE *stream) {
+static int uartPutchar(char c, FILE *stream) {
     Serial.write(c);
     return 0;
 }
@@ -172,7 +169,7 @@ void lightFaceFromHex(char * hexstring){
 void setup() {
     // Initialize serial and printf
     Serial.begin(9600);
-    fdev_setup_stream(&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
+    fdev_setup_stream(&uartout, uartPutchar, NULL, _FDEV_SETUP_WRITE);
     stdout = &uartout;
     
     for (int i=0; i<16; i++) {
@@ -180,9 +177,9 @@ void setup() {
         digitalWrite(led_pins[i], HIGH); // All off
     }
 
-    output_face(states[0].face);
+    faceOutput(states[0].face);
     
-    /*/ test segments
+    /*/ Test segments
     int i=0;
     while (1) {
         digitalWrite(led_pins[i], HIGH);
@@ -191,30 +188,29 @@ void setup() {
         delay(500);
     } //*/
     
-    // Initial behaviour
-    states[0] = {0x5555, 10, 0};
-    states[1] = {0xAAAA, 10, 0};
+    // Test initial behaviour
+    states[0] = {0b1101000001101011, 10, 0};
+    states[1] = {0b1001000110010101, 10, 0};
     state_count=2;
-    transitions[0] = {0, 1, 1};
-    transitions[1] = {1, 1, 0};
-    transition_count=2;
+    transitions[0] = {0, 1, 30};
+    transitions[1] = {1, 0, 10};
+    transitions[2] = {0, 0, 10};
+    transitions[3] = {1, 1, 10};
+    transition_count=4;
     // */
 }
 
 void loop() {
     char input[80];
-    face_advance();
     if (readyLine(input, sizeof(input))) {
         if (startsWith("face ", input)) {
             lightFaceFromHex(&input[5]);
-        } else if (startsWith("on ", input)) {
+        } else if (startsWith("on ", input)) { // To remove
             digitalWrite(led_pins[atoi(&input[2])], LOW);
-        } else if (startsWith("off ", input)) {
+        } else if (startsWith("off ", input)) { // To remove
             digitalWrite(led_pins[atoi(&input[3])], HIGH);
         } else if (startsWith("echo", input)) {
             echo = !echo;
-        } else if (startsWith("events", input)) {
-            digitalWrite(13, LOW);
         } else if (startsWith("speed ", input)) {
             speed = strtol(&input[6], NULL, 10);
         } else if (startsWith("clear", input)) {
@@ -226,5 +222,6 @@ void loop() {
             printf("Invalid command. Try face, echo, events, state.\r\n");
         }
     }
+    faceAdvance();
     delay(speed);
 }
